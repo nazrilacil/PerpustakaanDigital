5e840832885fe2667ba57f14b1178ac6d6230067
Nazril Acil <nazrilacil04@gmail.com>
2025-07-03 20:06:33 +0700

    Update BookmarkView.vue

--- src/views/BookmarkView.vue
+++ src/views/BookmarkView.vue
@@ -1,6 +1,11 @@
-<!-- src/views/BookmarkView.vue -->
 <template>
   <div class="max-w-5xl mx-auto p-4">
+  <router-link
+      to="/"
+      class="inline-flex items-center gap-2 text-sm text-blue-600 hover:text-blue-800 font-medium mb-6 transition">
+      <SquareArrowLeft class="w-5 h-5" />
+      Kembali ke Beranda
+    </router-link>
     <h1 class="text-2xl font-bold mb-4">Bookmark Saya</h1>
     <div v-if="bookmarks.length === 0" class="text-gray-500 italic">Belum ada buku ditandai.</div>
     <div v-else class="grid grid-cols-2 lg:grid-cols-3 gap-4">
